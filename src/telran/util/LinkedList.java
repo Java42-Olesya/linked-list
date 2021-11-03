a0c62322f1be9c59238b7dd03f90c640c7bec7e0
Olesya <petrenco.olesya@gmail.com>
2021-11-03 09:06:39 +0200

    hw # 9 (without sort)

--- src/telran/util/LinkedList.java
+++ src/telran/util/LinkedList.java
@@ -33,9 +33,10 @@ public class LinkedList<T> implements List<T> {
 		size++;
 
 	}
+
 	private Node<T> getNode(int index) {
 		Node<T> res = null;
-		if(isValidIndex(index)) {
+		if (isValidIndex(index)) {
 			res = index <= size / 2 ? getNodefromLeft(index) : getNodeFromRight(index);
 		}
 		return res;
@@ -44,25 +45,28 @@ public class LinkedList<T> implements List<T> {
 	private Node<T> getNodeFromRight(int index) {
 		Node<T> current = tail;
 		int ind = size - 1;
-		while(ind != index) {
+		while (ind != index) {
 			ind--;
 			current = current.prev;
 		}
 		return current;
 	}
+
 	private Node<T> getNodefromLeft(int index) {
 		Node<T> current = head;
 		int ind = 0;
-		while(ind != index) {
+		while (ind != index) {
 			ind++;
 			current = current.next;
 		}
 		return current;
 	}
+
 	private boolean isValidIndex(int index) {
-		
-		return index >=0 && index < size;
+
+		return index >= 0 && index < size;
 	}
+
 	@Override
 	public boolean add(int index, T element) {
 		boolean res = false;
@@ -86,17 +90,19 @@ public class LinkedList<T> implements List<T> {
 		Node<T> nodeAfter = getNode(index);
 		newNode.next = nodeAfter;
 		newNode.prev = nodeAfter.prev;
-		//nodeAfter.prev => reference to the old previous element
+		// nodeAfter.prev => reference to the old previous element
 		nodeAfter.prev.next = newNode;
 		nodeAfter.prev = newNode;
-		
+
 	}
+
 	private void addHead(Node<T> newNode) {
 		newNode.next = head;
 		head.prev = newNode;
 		head = newNode;
-		
+
 	}
+
 	@Override
 	public int size() {
 
@@ -115,26 +121,70 @@ public class LinkedList<T> implements List<T> {
 
 	@Override
 	public T remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!isValidIndex(index)) {
+			return null;
+		}
+		Node<T> resNode = getNode(index);
+		T res = resNode.obj;
+		if (head == tail) {
+			head = tail = null;
+
+		} else {
+			if (index == 0) {
+				head = resNode.next;
+				head.prev = null;
+			} else if (index == size - 1) {
+				tail = resNode.prev;
+				tail.next = null;
+
+			} else {
+				resNode.prev.next = resNode.next;
+				resNode.next.prev = resNode.prev;
+			}
+		}
+		size--;
+		return res;
 	}
 
 	@Override
 	public int indexOf(Predicate<T> predicate) {
-		// TODO Auto-generated method stub
-		return 0;
+		int index = -1;
+		Node<T> resNode = head;
+		for (int i = 0; i < size; i++) {
+			if (predicate.test(resNode.obj)) {
+				index = i;
+				break;
+			}
+			resNode = resNode.next;
+		}
+		return index;
 	}
 
 	@Override
 	public int lastIndexOf(Predicate<T> predicate) {
-		// TODO Auto-generated method stub
-		return 0;
+		int index = -1;
+		Node<T> resNode = tail;
+		for (int i = size - 1; i >= 0; i--) {
+			if (predicate.test(resNode.obj)) {
+				index = i;
+				break;
+			}
+			resNode = resNode.prev;
+		}
+		return index;
 	}
 
 	@Override
 	public boolean removeIf(Predicate<T> predicate) {
-		// TODO Auto-generated method stub
-		return false;
+		int oldSize = size;
+		Node<T> resNode = tail;
+		for (int i = size - 1; i >= 0; i--) {
+		if (predicate.test(resNode.obj)) {
+			remove(i);
+		}
+			resNode = resNode.prev;
+	}
+		return oldSize > size;
 	}
 
 	@Override
@@ -142,19 +192,20 @@ public class LinkedList<T> implements List<T> {
 		T[] array = listToArray();
 		Arrays.sort(array, comp);
 		fillListFromArray(array);
-		
 
 	}
+
 	private T[] listToArray() {
-		//TODO
-		//creates array of T objects
-		//passes over whole list and fills the array
-		//sorting filled array
+		// TODO
+		// creates array of T objects
+		// passes over whole list and fills the array
+		// sorting filled array
 		return null;
 	}
+
 	private void fillListFromArray(T[] array) {
-		//TODO
-		//passes over whole list and fills elements from index=0 to index=size - 1 
+		// TODO
+		// passes over whole list and fills elements from index=0 to index=size - 1
 	}
 
 }
